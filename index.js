69784286803d2ca7b0bf7a20929601e55d3eb349
Ambar <Ambarpg80@gmail.com>
2022-10-06 22:46:02 -0400

    commit tree d852eb7ac296a16679a1c68233a137df0b241f65

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const eliminate = /[^A-Za-z0-9]/g;
+  let forwards = word.toLowerCase().replace( eliminate, '');
+  let backwards = forwards.split('').reverse().join('');
+  return forwards === backwards
 }
 
 /* 
   Add your pseudocode here
+  - make everything lowercase
+  -replace unwanted characters
+  - reverse order of spelling 
+  - check that the forwards vs backwards spellings and
+  - return true or flase
 */
 
 /*
   Add written explanation of your solution here
+  Write a function that results in a boolean return after checking if a word is the same spelled forwards as it is spelled backwards.
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,8 +25,12 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("morning"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
